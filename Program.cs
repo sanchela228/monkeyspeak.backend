9598cb5e86170f931451cdb1977133cda71cd083
bessonov <nikwoochach@yandex.ru>
2025-05-11 15:05:08 +0500

    Add SSL

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(serverOptions => {
+    serverOptions.ConfigureHttpsDefaults(httpsOptions => {
+        httpsOptions.SslProtocols = System.Security.Authentication.SslProtocols.Tls12;
+    });
+});
+
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
