c1511bd4564b0b5262766edad2720644c4e7ac42
vulf635 <vulf645@gmail.com>
2025-08-29 12:05:45 +0500

    some changes

--- Core/Database/Context.cs
+++ Core/Database/Context.cs
@@ -6,7 +6,8 @@ public class Context : DbContext
 {
     public DbSet<Models.Session> Sessions { get; set; }
     public DbSet<Models.Application> Applications { get; set; }
-    
+    public DbSet<Models.Registration> Registrations { get; set; }
+
     public Context(DbContextOptions<Context> options) : base(options)
     {
     }
